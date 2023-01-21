1e3710e52bcbae6d7aaad9fca074a209adcfb07b
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-21 23:26:45 +0300

    feat: add errors array in error util

--- apps/server/src/utils/api-error-util.ts
+++ apps/server/src/utils/api-error-util.ts
@@ -1,17 +1,22 @@
+import { ValidationError } from 'express-validator';
+
 class ApiError extends Error {
   status: number;
 
-  constructor(status: number, message: string) {
+  errors: ValidationError[];
+
+  constructor(status: number, message: string, errors: ValidationError[] = []) {
     super(message);
     this.status = status;
+    this.errors = errors;
   }
 
   static UnauthorizedError() {
     return new ApiError(401, 'Unauthorized');
   }
 
-  static BadRequest(message: string) {
-    return new ApiError(400, message);
+  static BadRequest(message: string, errors: ValidationError[] = []) {
+    return new ApiError(400, message, errors);
   }
 }
 
