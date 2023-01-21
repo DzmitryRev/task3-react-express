c2bab98f24228466ee80fffd7bac3dd046883cb5
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-21 23:29:24 +0300

    refactor: add default params for registationDate and status

--- apps/server/src/models/user-model.ts
+++ apps/server/src/models/user-model.ts
@@ -13,8 +13,8 @@ const UserSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  status: { type: String, required: true },
-  registrationDate: { type: Date, required: true },
+  status: { type: String, required: true, default: 'active' },
+  registrationDate: { type: Date, required: true, default: new Date() },
   lastVisitDate: { type: Date, default: null },
 });
 
