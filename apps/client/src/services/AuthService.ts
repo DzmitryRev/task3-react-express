4df72292229b412f4e03664ec909e0da51b93a0c
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 01:28:14 +0300

    feat: user and auth services (no implementation)

---
+++ apps/client/src/services/AuthService.ts
@@ -0,0 +1,11 @@
+export function signup(name: string, email: string, password: string) {
+  // logic
+}
+
+export function signin(email: string, password: string) {
+  // logic
+}
+
+export function signout() {
+  // logic
+}
