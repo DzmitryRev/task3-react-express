9df6c94c9296e21b1798211e153b68ae8f803308
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-21 15:59:21 +0300

    feat: connect mongoDB
    - install: mongoose, dotenv, cors;
    - set up mongodb;
    - add .env;

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,13 +1,30 @@
 import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.set('strictQuery', false);
 
 const app: Express = express();
 
-const PORT = 4040;
+const PORT = process.env.PORT || 3003;
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is running. PORT=${PORT}`);
-});
+const start = async () => {
+  try {
+    const mongoUrl = process.env.MONGO_DB_URL;
+    if (!mongoUrl) throw new Error("Could't find access link to DB");
+    await mongoose.connect(mongoUrl).catch((error: Error) => {
+      throw new Error(error.message);
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running. PORT=${PORT}`);
+    });
+  } catch (error) {
+    console.log(error, 'a');
+  }
+};
+
+start();
