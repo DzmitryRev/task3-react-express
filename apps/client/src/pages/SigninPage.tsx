24f07db41ccd8a4bbb7f3caf45f0e52c4635cb52
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 19:53:49 +0300

    feat: implement and style login and reg. pages

--- apps/client/src/pages/SigninPage.tsx
+++ apps/client/src/pages/SigninPage.tsx
@@ -1,51 +1,86 @@
-import React from 'react';
-import { Button, TextField } from '@mui/material';
+import React, { useContext, useEffect } from 'react';
+import { Box, Button, TextField } from '@mui/material';
 import { useFormik } from 'formik';
-import { signUpValidationSchema as validationSchema } from '../yup/Schemas';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInValidationSchema as validationSchema } from '../yup/Schemas';
+import useRequestFlags from '../hooks/useRequestFlags';
+import AuthContext from '../context/AuthContext';
+import AuthService from '../services/AuthService';
+import globalStyles from '../styles/global';
+
+const formikInitialValue = {
+  email: '',
+  password: '',
+};
 
 export default function SigninPage() {
+  const { isAuth, setIsAuth, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/');
+    }
+  }, [isAuth]);
+
+  const { error, setError } = useRequestFlags();
+
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      password: '',
-    },
+    initialValues: formikInitialValue,
     validationSchema,
     onSubmit: (values) => {
       const { email, password } = values;
-      console.log(email, password);
-      // logic
+      setError('');
+      AuthService.signin(email, password)
+        .then((res) => {
+          localStorage.setItem('token', res.data.accessToken);
+          setUser(res.data.user);
+          setIsAuth(true);
+        })
+        .catch((e) => {
+          setError(e.response.data.message);
+        });
     },
   });
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
-      </form>
-    </div>
+    <Box sx={globalStyles.auth}>
+      <Box sx={globalStyles.authInnerContainer}>
+        <Box sx={globalStyles.authError}>{error}</Box>
+        <Box sx={{ mb: 4 }}>
+          <Link to="/signup">Регистрация</Link>
+        </Box>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            sx={{ mb: 2 }}
+            fullWidth
+            id="outlined-error"
+            name="email"
+            label={formik.touched.email ? formik.touched.email && formik.errors.email : 'Email'}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            autoComplete="off"
+          />
+          <TextField
+            sx={{ mb: 2 }}
+            fullWidth
+            id="outlined-error"
+            name="password"
+            label={
+              formik.touched.password
+                ? formik.touched.password && formik.errors.password
+                : 'Password'
+            }
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            autoComplete="off"
+          />
+          <Button color="primary" variant="contained" fullWidth type="submit">
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Box>
   );
 }
--- apps/client/src/pages/SignupPage.tsx
+++ apps/client/src/pages/SignupPage.tsx
@@ -1,63 +1,102 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useFormik } from 'formik';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { Link, useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material';
 import { signUpValidationSchema as validationSchema } from '../yup/Schemas';
+import AuthContext from '../context/AuthContext';
+import AuthService from '../services/AuthService';
+import useRequestFlags from '../hooks/useRequestFlags';
+import globalStyles from '../styles/global';
+
+const formikInitialValue = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 function SignupPage() {
+  const { isAuth, setIsAuth, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/');
+    }
+  }, [isAuth]);
+
+  const { error, setError } = useRequestFlags();
+
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      password: '',
-    },
+    initialValues: formikInitialValue,
     validationSchema,
     onSubmit: (values) => {
       const { name, email, password } = values;
-      console.log(name, email, password);
-      // logic
+      setError('');
+      AuthService.signup(name, email, password)
+        .then((res) => {
+          localStorage.setItem('token', res.data.accessToken);
+          setUser(res.data.user);
+          setIsAuth(true);
+        })
+        .catch((e) => {
+          setError(e.response.data.message);
+        });
     },
   });
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="name"
-          name="name"
-          label="Name"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          error={formik.touched.name && Boolean(formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-        />
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Зарегистрироваться
-        </Button>
-      </form>
-    </div>
+    <Box sx={globalStyles.auth}>
+      <Box sx={globalStyles.authInnerContainer}>
+        <Box sx={globalStyles.authError}>{error}</Box>
+        <Box sx={{ mb: 4 }}>
+          <Link to="/signin">Войти</Link>
+        </Box>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            sx={{ mb: 2 }}
+            fullWidth
+            id="outlined-error"
+            name="name"
+            label={formik.touched.name ? formik.touched.name && formik.errors.name : 'Name'}
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            autoComplete="off"
+          />
+          <TextField
+            sx={{ mb: 2 }}
+            fullWidth
+            id="outlined-error"
+            name="email"
+            label={formik.touched.email ? formik.touched.email && formik.errors.email : 'Email'}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            autoComplete="off"
+          />
+          <TextField
+            sx={{ mb: 2 }}
+            fullWidth
+            id="outlined-error"
+            name="password"
+            label={
+              formik.touched.password
+                ? formik.touched.password && formik.errors.password
+                : 'Password'
+            }
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            autoComplete="off"
+          />
+          <Button color="primary" variant="contained" fullWidth type="submit">
+            Зарегистрироваться
+          </Button>
+        </form>
+      </Box>
+    </Box>
   );
 }
 
