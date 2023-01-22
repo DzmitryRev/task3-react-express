bfb24e38e809eb810af40f58c5754b929006ed8f
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 19:50:16 +0300

    fix: change errors messages in schemas

--- apps/client/src/yup/Schemas.ts
+++ apps/client/src/yup/Schemas.ts
@@ -3,16 +3,10 @@ import * as yup from 'yup';
 export const signUpValidationSchema = yup.object({
   name: yup.string().required('Name is required'),
   email: yup.string().email('Enter a valid email').required('Email is required'),
-  password: yup
-    .string()
-    .min(1, 'Password should be of minimum 1 characters length')
-    .required('Password is required'),
+  password: yup.string().required('Password is required'),
 });
 
-export const SignInvalidationSchema = yup.object({
-  email: yup.string().email('Enter a valid email').required('Email is required'),
-  password: yup
-    .string()
-    .min(1, 'Password should be of minimum 1 characters length')
-    .required('Password is required'),
+export const signInValidationSchema = yup.object({
+  email: yup.string().required('Email is required').email('Enter a valid email'),
+  password: yup.string().required('Password is required'),
 });
