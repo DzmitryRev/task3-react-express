1a840066b548f0ce0c8a6667f27062800a81105c
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 16:09:45 +0300

    feat: implement auth service with main requests

--- apps/client/src/services/AuthService.ts
+++ apps/client/src/services/AuthService.ts
@@ -1,11 +1,18 @@
-export function signup(name: string, email: string, password: string) {
-  // logic
+import api from '../fetch';
+import { IAuthRes } from '../types/AuthResTypes';
+
+class AuthService {
+  static signup(name: string, email: string, password: string) {
+    return api.post<IAuthRes>('/signup', { name, email, password });
+  }
+
+  static signin(email: string, password: string) {
+    return api.post<IAuthRes>('/signin', { email, password });
+  }
+
+  static signout() {
+    return api.post('/signout');
+  }
 }
 
-export function signin(email: string, password: string) {
-  // logic
-}
-
-export function signout() {
-  // logic
-}
+export default AuthService;
