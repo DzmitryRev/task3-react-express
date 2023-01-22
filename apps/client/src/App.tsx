73e79f56cd31c123d910e90bf1ff8c3e2eb8ba9c
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 16:10:51 +0300

    feat: implement router

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
+import UsersPage from './pages/UsersPage';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index path="/" element={<UsersPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="signin" element={<SigninPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
