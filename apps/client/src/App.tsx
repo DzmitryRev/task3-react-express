25190aa23df6f540e94c7486a1b457ec9e642eca
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 19:53:03 +0300

    feat: set up context proveder

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import AuthContext from './context/AuthContext';
+import { URL } from './fetch';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
 import UsersPage from './pages/UsersPage';
+import AuthService from './services/AuthService';
+import { IAuthRes } from './types/AuthResTypes';
+import { IUser } from './types/UserTypes';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get<IAuthRes>(`${URL}/refresh`, { withCredentials: true }).then((res) => {
+      setIsAuth(true);
+      setUser(res.data.user);
+    }).catch(() => {
+      navigate('/signin');
+    });
+  }, []);
+
+  const authContextProviderValue = useMemo(
+    () => ({
+      isAuth,
+      setIsAuth,
+      user,
+      setUser,
+    }),
+    [isAuth, setIsAuth, user, setUser],
+  );
   return (
     <div className="App">
-      <Routes>
-        <Route index path="/" element={<UsersPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="signin" element={<SigninPage />} />
-      </Routes>
+      <header>
+        {user && user.email}
+        {isAuth && (
+          <button
+            type="button"
+            onClick={() => {
+              AuthService.signout().then(() => {
+                setIsAuth(false);
+                setUser(null);
+                localStorage.removeItem('token');
+              });
+            }}
+          >
+            Выйти
+          </button>
+        )}
+      </header>
+      <AuthContext.Provider value={authContextProviderValue}>
+        <Routes>
+          <Route index path="/" element={<UsersPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="signin" element={<SigninPage />} />
+        </Routes>
+      </AuthContext.Provider>
     </div>
   );
 }
