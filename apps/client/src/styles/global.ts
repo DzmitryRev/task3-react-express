6e8be3507a3133a2d9fb6f4bcca37ca5d180b878
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 19:52:40 +0300

    feat: add object with global styles MUI

---
+++ apps/client/src/styles/global.ts
@@ -0,0 +1,11 @@
+const globalStyles = {
+  auth: { display: 'flex', alignItems: 'center', minHeight: '100vh' },
+  authInnerContainer: { position: 'relative', mx: 'auto', maxWidth: '500px' },
+  authError: {
+    position: 'absolute',
+    right: 0,
+    color: 'red',
+  },
+};
+
+export default globalStyles;
