cbbe3471953835dc09c06ff316c90382e39d5e73
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-22 16:09:16 +0300

    feat: install formik and impl. skeleton of sign -up/-in pages

--- apps/client/src/pages/SigninPage.tsx
+++ apps/client/src/pages/SigninPage.tsx
@@ -1,9 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Button, TextField } from '@mui/material';
+import { useFormik } from 'formik';
+import { signUpValidationSchema as validationSchema } from '../yup/Schemas';
 
 export default function SigninPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      const { email, password } = values;
+      console.log(email, password);
+      // logic
+    },
+  });
+
   return (
-    <div>Войти</div>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 }
--- apps/client/src/pages/SignupPage.tsx
+++ apps/client/src/pages/SignupPage.tsx
@@ -1,10 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { signUpValidationSchema as validationSchema } from '../yup/Schemas';
 
-export default function SignunPage() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function SignupPage() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      const { name, email, password } = values;
+      console.log(name, email, password);
+      // logic
+    },
+  });
+
   return (
-    <div>Загеристироваться</div>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Зарегистрироваться
+        </Button>
+      </form>
+    </div>
   );
 }
+
+export default SignupPage;
